64c9f8530d32a13c0b5031f3e2957dba0707221f
focso5 <focso5@gmail.com>
2021-01-22 21:40:07 +0900

    pop menu js 연결

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
 (function (window, document) {
-    'use strict';
+  'use strict';
 
+  // 1. 배열처럼 값을 가져오지만 배열로 사용할 수 없음.
+  const $mouseOver = document.querySelectorAll('.mouseOver');
 
-    
+  const $gnb_pop = document.getElementById('gnb-pop');
+  const $center_pop = document.getElementById('center-pop');
+  const $loc_pop = document.getElementById('loc-pop');
+  //console.log(mouseOver.length);
 
-    const $gnb_menu_on = document.getElementById('gnb-all');
-    const $gnb_menuOff = document.getElementById('sub-menu__in');
+  // 2. 배열로 변경.
+  const _arr = Array.from($mouseOver);
+  _arr.forEach((item) => {
+    item.addEventListener('mouseover', menuOpen);
 
-    const $sub_menu = document.getElementById('gnb__sub');
+    item.addEventListener('mouseout', menuClose);
+  });
 
-    $gnb_menu_on.addEventListener('mouseenter', function () {
-        // alert("야호");
-        menuOpen();
-    });
-    $gnb_menu_on.addEventListener('mouseout', function () {
-        menuClose();
-    });
+  function menuOpen(e) {
+    //console.log(e.target.id);
 
-    function menuOpen() {
-        $sub_menu.style.display = 'block';
+    switch (e.target.id) {
+      case 'menuAll':
+        $gnb_pop.classList.toggle('on');
+        break;
+
+      case 'center':
+        $center_pop.classList.toggle('on');
+        break;
+
+      case 'location':
+        $loc_pop.classList.toggle('on');
+        break;
     }
-    function menuClose() {
-        setTimeout(function() {
-            $sub_menu.style.display = 'none';
-        },500);
-    };
-})(window, document)
+  }
+
+  function menuClose(e) {
+    console.log('g');
+    switch (e.target.id) {
+      case 'gnb-pop':
+        $gnb_pop.classList.toggle('off');
+        break;
+
+      case 'center-pop':
+        $center_pop.classList.toggle('off');
+        break;
+
+      case 'loc-pop':
+        $loc_pop.classList.toggle('off');
+        break;
+    }
+  }
+})(window, document);
